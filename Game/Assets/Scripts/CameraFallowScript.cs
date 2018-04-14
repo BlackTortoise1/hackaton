9a620194e35c58ccfc188b94b6991f7180bc7605
kukka55 <ralucachange95@yahoo.com>
2018-04-14 15:34:57 +0300

    MouseOY move
    look up and down on mouse input

--- Game/Assets/Scripts/CameraFallowScript.cs
+++ Game/Assets/Scripts/CameraFallowScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CameraFallowScript : MonoBehaviour {
 
     public GameObject player;       //Public variable to store a reference to the player game object
+    public float verticalSpeed = 0.5F;
+    public float maxOYOffset = 1.1F;
+    public float minOYOffset = -1.5F;
+
+    private float OYOffset = 0;
 
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
@@ -20,8 +25,13 @@ public class CameraFallowScript : MonoBehaviour {
     void Update()
     {
         transform.position = player.transform.TransformPoint(offset);
+        
+        float OxRot = verticalSpeed * Input.GetAxis("Mouse Y");
+        OYOffset += OxRot;
+        print(OYOffset);
+        OYOffset = Mathf.Clamp(OYOffset, minOYOffset, maxOYOffset);
 
-        transform.LookAt(player.transform.position);
+        transform.LookAt(player.transform.position + new Vector3(0, OYOffset, 0));
 
     }
 }
